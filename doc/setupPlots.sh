ef2d37e692616f0d714dea768999afdabc4f7ffd
David Adams <dladams@bnl.gov>
2020-05-21 11:51:54 -0500

    Update dos.

--- doc/setupPlots.sh
+++ doc/setupPlots.sh
@@ -1,7 +1,13 @@
+# setupPlots.sh
+#
+# David Adams
+# May 2019
+#
+# Example configuration file for the drawNoise command in dunenoise.
+
 SDET=iceberg3
 PLOTS="tai-nsgrms:300,tai-nsgrms50:200,cnr-nsgrms:300,cnr-nsgrms50:200"
 CRVS="zcGood-uvGood"
-#DIRPAT="dpn_%REC%ke1/iceberg%RUN%/chmet_nsgrms%NSAM%_all_run%RUN%.tpad"
 DIRPAT="ib%REC%NoiseVsChan/iceberg%RUN%/chmet_%VAR%_all_run%RUN%.tpad"
 GRPPAT="ib%REC%NoiseVsChan/ibgroup%RUN%/chmet_%VAR%_all_run%RUN1%.tpad"
 OUTPRE="plots-noisesum/"
