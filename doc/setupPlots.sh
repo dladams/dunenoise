916e5a0b987447ed53680f63ec3a02afd09d5fd5
David Adams <dladams@bnl.gov>
2020-05-21 11:38:09 -0500

    Add example config script for drawNoise.

---
+++ doc/setupPlots.sh
@@ -0,0 +1,7 @@
+SDET=iceberg3
+PLOTS="tai-nsgrms:300,tai-nsgrms50:200,cnr-nsgrms:300,cnr-nsgrms50:200"
+CRVS="zcGood-uvGood"
+#DIRPAT="dpn_%REC%ke1/iceberg%RUN%/chmet_nsgrms%NSAM%_all_run%RUN%.tpad"
+DIRPAT="ib%REC%NoiseVsChan/iceberg%RUN%/chmet_%VAR%_all_run%RUN%.tpad"
+GRPPAT="ib%REC%NoiseVsChan/ibgroup%RUN%/chmet_%VAR%_all_run%RUN1%.tpad"
+OUTPRE="plots-noisesum/"
